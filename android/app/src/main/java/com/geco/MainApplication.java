f1580a3b35ccb559026f0df17c21a1976111652c
Abduzarif Madraimov <zarifmad@gmail.com>
2019-05-03 12:13:57 +0600

    installing Maps api and directions api

--- android/app/src/main/java/com/geco/MainApplication.java
+++ android/app/src/main/java/com/geco/MainApplication.java
@@ -3,6 +3,7 @@ package com.geco;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 import com.thebylito.navigationbarcolor.NavigationBarColorPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new VectorIconsPackage(),
             new RNCViewPagerPackage(),
             new NavigationBarColorPackage()
