ef7bcbf24b32e391062fd9d7f4aa02116ddf392c
jinyung <jinyungisnot@gmail.com>
2019-05-03 14:55:19 +0800

    added app directory and added dummy files

---
+++ app/components/GoogleMaps.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: 960,
+        width: 500,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
+
+export default class GoogleMaps extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <View style={styles.container}>
+                <MapView
+                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                    style={styles.map}
+                    region={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+                </MapView>
+            </View>
+        );
+    }
+}
