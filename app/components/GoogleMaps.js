78d7ab9a9cce4c063281591b92c4238ddb16fefa
jinyung <jinyungisnot@gmail.com>
2019-05-06 16:43:52 +0800

    added IntroSlider, TODO implement it to app.js

--- app/components/GoogleMaps.js
+++ app/components/GoogleMaps.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const styles = StyleSheet.create({
     container: {
@@ -12,6 +13,12 @@ const styles = StyleSheet.create({
     },
     map: {
         ...StyleSheet.absoluteFillObject,
+    },
+    overlay: {
+        position: 'absolute',
+        bottom: 400,
+        alignItems: 'center',
+        width: '100%',
     },
 });
 
@@ -32,8 +39,42 @@ export default class GoogleMaps extends Component {
                         latitudeDelta: 0.015,
                         longitudeDelta: 0.0121,
                     }}
-                >
-                </MapView>
+                />
+                <TouchableOpacity style={styles.overlay}>
+                    <GooglePlacesAutocomplete
+                        placeholder='Enter Location'
+                        minLength={2}
+                        autoFocus={false}
+                        returnKeyType={'default'}
+                        fetchDetails={true}
+                        query={{
+                            // available options: https://developers.google.com/places/web-service/autocomplete
+                            key: 'AIzaSyBzkPNDzDRYBBETcA6QJV5JSJi61aofwtk',
+                            language: 'en', // language of the results
+                            types: '(cities)' // default: 'geocode'
+                        }}
+
+                        styles={{
+                            textInputContainer: {
+                                backgroundColor: 'rgba(0,0,0,0)',
+                                borderTopWidth: 0,
+                                borderBottomWidth:0,
+                                width: '100%',
+                            },
+                            textInput: {
+                                marginLeft: 0,
+                                marginRight: 0,
+                                height: 38,
+                                color: '#5d5d5d',
+                                fontSize: 16,
+                            },
+                            predefinedPlacesDescription: {
+                                color: '#1faadb'
+                            },
+                        }}
+                        currentLocation={false}
+                    />
+                </TouchableOpacity>
             </View>
         );
     }
---
+++ app/styles/style.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    viewPager: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textIntro: {
+        fontFamily: 'SFCompactDisplayBold',
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#43A047',
+        alignSelf: 'center',
+    },
+    containerView: {
+        justifyContent: 'center',
+        flex: 1,
+    },
+    colorView: {
+        justifyContent: 'center',
+    },
+    centerIcon: {
+        alignSelf: 'center',
+    }
+});
