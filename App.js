384e69e9d4cce4dde776223dd27380e49aaafe08
jinyung <jinyungisnot@gmail.com>
2019-05-06 16:43:52 +0800

    added IntroSlider, TODO implement it to app.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+<<<<<<< master
 import { StyleSheet, View, Text, StatusBar } from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
 import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
@@ -106,3 +107,42 @@ const slides = [
         backgroundColor: '#AED581',
     },
 ];
+=======
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Playground from './app/containers/Playground';
+import Home from './app/containers/Home';
+import IntroSlider from './app/containers/IntroSlider';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
+const TabNavigator = createBottomTabNavigator(
+    {
+        Home: Home,
+        Playground: IntroSlider,
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                let IconComponent = Ionicons;
+                let iconName;
+                if (routeName === 'Home') {
+                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                    // Sometimes we want to add badges to some icons.
+                    // You can check the implementation below.
+                } else if (routeName === 'Playground') {
+                    iconName = `ios-options`;
+                }
+
+                // You can return any component that you like here!
+                return <IconComponent name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        },
+    }
+);
+
+export default createAppContainer(TabNavigator);
+>>>>>>> added IntroSlider, TODO implement it to app.js
