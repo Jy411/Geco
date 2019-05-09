b6b795f99af6ce6102d09df39b6ee73256ba0d76
jinyung <jinyungisnot@gmail.com>
2019-05-09 18:51:24 +0800

    Merge branch 'master' of https://github.com/Jy411/Geco into jy-geco
    # Conflicts:
    #	App.js
    #	app/components/Map.js
    #	package.json
    
    - Added IntroSlider

--- App.js
+++ App.js
@@ -1,38 +1,108 @@
 import React, {Component} from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Playground from './app/containers/Playground';
-import Home from './app/containers/Home';
-import IntroSlider from './app/containers/IntroSlider';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
-const TabNavigator = createBottomTabNavigator(
+export default class App extends Component<Props, State> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // To determine whether to show the introduction sliders
+            showRealApp: false,
+        };
+        changeNavigationBarColor("#689F38");
+        StatusBar.setBackgroundColor("#689F38");
+    }
+
+
+    _onDone = () => {
+        this.setState({showRealApp: true});
+    };
+
+    _onSkip = () => {
+        this.setState({showRealApp: true});
+    };
+
+    render() {
+        if (this.state.showRealApp) {
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: 50,
+                    }}>
+                    <Text>
+                        This will be your screen when you click Skip from any slide or Done
+                        button at last
+                    </Text>
+                </View>
+            );
+        } else {
+            return (
+                <AppIntroSlider
+                    slides={slides}
+                    //coming from the JsonArray below
+                    onDone={this._onDone}
+                    //Handler for the done On last slide
+                    showSkipButton={true}
+                    onSkip={this._onSkip}
+                />
+            );
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    image: {
+        // width: 250,
+        height: hp("70%"),
+        bottom: hp("5%"),
+        resizeMode: 'center',
+    },
+    sliderText: {
+        color: '#FFFFFF',
+        fontSize: 28,
+        bottom: hp("5%"),
+        textAlign: 'center',
+        textAlignVertical: 'top',
+        fontWeight: 'bold',
+    },
+});
+
+const slides = [
     {
-        Home: Home,
-        Playground: IntroSlider,
+        key: 's1',
+        text: 'Welcome to Geco',
+        textStyle: styles.sliderText,
+        image: require('./app/images/HDTree1.png'),
+        imageStyle: styles.image,
+        backgroundColor: '#AED581',
     },
     {
-        defaultNavigationOptions: ({ navigation }) => ({
-            tabBarIcon: ({ focused, horizontal, tintColor }) => {
-                const { routeName } = navigation.state;
-                let IconComponent = Ionicons;
-                let iconName;
-                if (routeName === 'Home') {
-                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-                    // Sometimes we want to add badges to some icons.
-                    // You can check the implementation below.
-                } else if (routeName === 'Playground') {
-                    iconName = `ios-options`;
-                }
-
-                // You can return any component that you like here!
-                return <IconComponent name={iconName} size={25} color={tintColor} />;
-            },
-        }),
-        tabBarOptions: {
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-        },
-    }
-);
-
-export default createAppContainer(TabNavigator);
+        key: 's2',
+        text: 'Geco helps measure your impact on the environment',
+        textStyle: styles.sliderText,
+        image: require('./app/images/HDTree3.png'),
+        imageStyle: styles.image,
+        backgroundColor: '#AED581',
+    },
+    {
+        key: 's3',
+        text: 'Each small step brings us closer to our goal',
+        textStyle: styles.sliderText,
+        image: require('./app/images/HDTree5.png'),
+        imageStyle: styles.image,
+        backgroundColor: '#AED581',
+    },
+    {
+        key: 's4',
+        text: 'The best time to plant a tree was 20 years ago. The second best time is now.',
+        textStyle: styles.sliderText,
+        image: require('./app/images/earth.png'),
+        imageStyle: styles.image,
+        backgroundColor: '#AED581',
+    },
+];
