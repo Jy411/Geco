817c0f13ea3f3164c75bfab1db7e174a1b86cd07
Abduzarif Madraimov <zarifmad@gmail.com>
2019-06-03 16:27:42 +0600

    Get current location

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { AppRegistry, StyleSheet, Dimensions, View, Text } from "react-native";
+import {AppRegistry, StyleSheet, Dimensions, View, Text, Platform} from "react-native";
 
 import MapView from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
@@ -9,16 +9,16 @@ import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete"
 
 
 type State = {
+    lat:number,
+    long:number,
     type:string;
     distance:number;
     duration:number;
     destination:string;
-    origin:string;
     oLat:number;
     oLong:number;
     dLat:number;
     dLong:number;
-    oExists:boolean;
     dExists:boolean;
 }
 
@@ -31,10 +31,12 @@ class Map extends React.Component<Props, State> {
 
 
         this.state = {
+            lat:0,
+            lang:0,
             distance:0,
             duration:0,
             type:'',
-            origin:'',
+
 
             oLat: 0,
 
@@ -45,19 +47,33 @@ class Map extends React.Component<Props, State> {
             dLong: 0,
             error:null,
             destination:'',
-            oExists: false,
+
             dExists: false,
         };
 
     }
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    lat: position.coords.latitude,
+                    long: position.coords.longitude,
+                    error: null,
+                });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: false, timeout: 30000 },
+        );
+    }
 
     render() {
-        const origin = {latitude: this.state.oLat, longitude: this.state.oLong};
+        const origin = {latitude: this.state.lat, longitude: this.state.long};
         const destinations = {latitude: this.state.dLat, longitude: this.state.dLong};
         return (
                 <View style={{width:'100%'}}>
-                    <Text style={{top:40, position:'absolute'}}>{this.state.duration}</Text>
-                    <Text style={{top:80, position:'absolute'}}>{this.state.distance}</Text>
+
+                    <Text style={{bottom:40, position:'absolute'}}>{this.state.duration}</Text>
+                    <Text style={{bottom:80, position:'absolute'}}>{this.state.distance}</Text>
                     <GooglePlacesAutocomplete
                         placeholder='Enter Location'
                         minLength={2}
@@ -65,7 +81,7 @@ class Map extends React.Component<Props, State> {
                         returnKeyType={'default'}
                         fetchDetails={true}
                         onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-                            this.setState({ origin: details.formatted_address, oLat:details.geometry.location.lat, oLong:details.geometry.location.lng, oExists:true })
+                            this.setState({destination: details.formatted_address, dLat:details.geometry.location.lat, oLong:details.geometry.location.lng, dExists:true })
 
                         }}
                         query={{
@@ -98,78 +114,78 @@ class Map extends React.Component<Props, State> {
                                 color: '#1faadb'
                             },
                         }}
-                        currentLocation={true}
-                    />
-                    <GooglePlacesAutocomplete
-                        placeholder='Enter Location'
-                        minLength={2}
-                        autoFocus={false}
-                        returnKeyType={'default'}
-                        fetchDetails={true}
-                        onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-                            this.setState({ destination: details.formatted_address, dLat:details.geometry.location.lat, oLong:details.geometry.location.lng, dExists:true })
-                        }}
-                        query={{
-                            // available options: https://developers.google.com/places/web-service/autocomplete
-                            key: 'AIzaSyAXB4arZesKpFxvYR8ZhE0zxhMJ5SZjjl8',
-                            language: 'en', // language of the results
-                        }}
-                        styles={{
-                            container:{
-                                top:44,
-                                width:'100%',
-                                height:80,
-                                position:'absolute'
-                            },
-                            textInputContainer: {
-                                top:80,
-                                backgroundColor: 'rgba(0,0,0,0)',
-                                borderTopWidth: 0,
-                                borderBottomWidth:0,
-                                width:"100%"
-                            },
-                            textInput: {
-                                marginLeft: 0,
-                                marginRight: 0,
-                                height: 36,
-                                color: '#5d5d5d',
-                                fontSize: 16
-                            },
-                            predefinedPlacesDescription: {
-                                color: '#1faadb'
-                            },
-                        }}
-                        currentLocation={false}
-                    />
-                    <MapView style={styles.map} initialRegion={{
-                        latitude: 3.0630348,
-                        longitude: 101.614712,
-                        latitudeDelta: 1,
-                        longitudeDelta: 1
-                    }}
 
+                    />
+                    {/*<GooglePlacesAutocomplete*/}
+                    {/*    placeholder='Enter Location'*/}
+                    {/*    minLength={2}*/}
+                    {/*    autoFocus={false}*/}
+                    {/*    returnKeyType={'default'}*/}
+                    {/*    fetchDetails={true}*/}
+                    {/*    onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true*/}
+                    {/*        this.setState({ destination: details.formatted_address, dLat:details.geometry.location.lat, oLong:details.geometry.location.lng, dExists:true })*/}
+                    {/*    }}*/}
+                    {/*    query={{*/}
+                    {/*        // available options: https://developers.google.com/places/web-service/autocomplete*/}
+                    {/*        key: 'AIzaSyAXB4arZesKpFxvYR8ZhE0zxhMJ5SZjjl8',*/}
+                    {/*        language: 'en', // language of the results*/}
+                    {/*    }}*/}
+                    {/*    styles={{*/}
+                    {/*        container:{*/}
+                    {/*            top:44,*/}
+                    {/*            width:'100%',*/}
+                    {/*            height:80,*/}
+                    {/*            position:'absolute'*/}
+                    {/*        },*/}
+                    {/*        textInputContainer: {*/}
+                    {/*            top:80,*/}
+                    {/*            backgroundColor: 'rgba(0,0,0,0)',*/}
+                    {/*            borderTopWidth: 0,*/}
+                    {/*            borderBottomWidth:0,*/}
+                    {/*            width:"100%"*/}
+                    {/*        },*/}
+                    {/*        textInput: {*/}
+                    {/*            marginLeft: 0,*/}
+                    {/*            marginRight: 0,*/}
+                    {/*            height: 36,*/}
+                    {/*            color: '#5d5d5d',*/}
+                    {/*            fontSize: 16*/}
+                    {/*        },*/}
+                    {/*        predefinedPlacesDescription: {*/}
+                    {/*            color: '#1faadb'*/}
+                    {/*        },*/}
+                    {/*    }}*/}
+                    {/*    currentLocation={false}*/}
+                    {/*/>*/}
+                    <MapView style={styles.map} initialRegion={{
+                        latitude: this.state.lat,
+                        longitude: this.state.long,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                             zoomEnabled={true}
                              showsCompass={true}
                              mapType={"standard"}
                              showsMyLocationButton={true}
                              showsPointsOfInterest={true}
                     >
 
+                        <MapMarker coordinate={{latitude:this.state.lat, longitude:this.state.long}} />
 
-                        {this.state.oExists && <MapMarker coordinate={{latitude:this.state.oLat, longitude:this.state.oLong}} />}
 
-                        {this.state.oExists && this.state.dExists && <MapMarker coordinate={{latitude:this.state.dLat, longitude:this.state.dLong}}/>}
+                        {this.state.dExists && <MapMarker coordinate={{latitude:this.state.dLat, longitude:this.state.dLong}}/>}
 
-                        {this.state.oExists && this.state.dExists &&
+
 
                         <MapViewDirections
-                            origin={this.state.origin}
+                            origin={origin}
                             destination={this.state.destination}
                             apikey={'AIzaSyAXB4arZesKpFxvYR8ZhE0zxhMJ5SZjjl8'}
                             mode={'driving'}
                             onReady={(result) => {
                                 this.setState({duration:result.duration, distance:result.distance})
                             }}
-                        />}
+                        />
 
                     </MapView>
                 </View>
@@ -211,9 +227,9 @@ const styles = StyleSheet.create({
 
         right: 0,
 
-        bottom: 120,
+        bottom: 0,
         width:'100%',
-        height:'100%',
+        height:'80%',
     },
 
 });
